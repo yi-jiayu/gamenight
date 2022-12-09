4b85e53d0737fb823e6b6aea56564bbf7614721b
Jiayu Yi <yijiayu@gmail.com>
2022-12-09 15:31:31 +0000

    Update CLI to print top N messages

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -12,15 +13,17 @@ import (
 )
 
 var (
-	apiKey     string
-	videoID    string
-	liveChatID string
+	apiKey        string
+	videoID       string
+	liveChatID    string
+	windowSeconds int
 )
 
 func init() {
 	flag.StringVar(&apiKey, "apiKey", "", "API key")
 	flag.StringVar(&videoID, "videoID", "", "Video ID")
 	flag.StringVar(&liveChatID, "liveChatID", "", "Live chat ID")
+	flag.IntVar(&windowSeconds, "windowSeconds", 60, "Sliding window duration in seconds")
 }
 
 func main() {
@@ -51,17 +54,45 @@ func main() {
 		liveChatID = res.Items[0].LiveStreamingDetails.ActiveLiveChatId
 	}
 
-	req := yt.LiveChatMessages.List(liveChatID, []string{"snippet"})
+	req := yt.LiveChatMessages.List(liveChatID, []string{"authorDetails", "snippet"})
+
+	config := LiveChatAggregatorConfig{
+		WindowDuration: time.Duration(windowSeconds) * time.Second,
+	}
+	aggregator := NewLiveChatAggregator(config)
+
 	for {
 		res, err := req.Do()
 		if err != nil {
 			panic(err)
 		}
+
+		var messages []Message
 		for _, message := range res.Items {
-			fmt.Println(message.Snippet.TextMessageDetails.MessageText)
+			if message.Snippet.TextMessageDetails == nil {
+				// ignore non-text messages
+				continue
+			}
+			timestamp, err := time.Parse("2006-01-02T15:04:05.999999-07:00", message.Snippet.PublishedAt)
+			if err != nil {
+				log.Printf("ignoring invalid timestamp: %v", message.Snippet.PublishedAt)
+				continue
+			}
+			messages = append(messages, Message{
+				Sender:    message.AuthorDetails.ChannelId,
+				Content:   message.Snippet.TextMessageDetails.MessageText,
+				Timestamp: timestamp,
+			})
 		}
-		fmt.Fprintf(os.Stderr, "wait %d ms\n", res.PollingIntervalMillis)
+		aggregator.Ingest(messages)
+		top10 := aggregator.TopN(10)
+		fmt.Print("\033[H\033[2J")
+		for _, message := range top10 {
+			fmt.Printf("%s: %d\n", message.Content, message.Count)
+		}
+		fmt.Printf("window_size=%d", len(aggregator.window))
+
 		time.Sleep(time.Duration(res.PollingIntervalMillis) * time.Millisecond)
-		req = yt.LiveChatMessages.List(liveChatID, []string{"snippet"}).PageToken(res.NextPageToken)
+		req = yt.LiveChatMessages.List(liveChatID, []string{"authorDetails", "snippet"}).PageToken(res.NextPageToken)
 	}
 }
